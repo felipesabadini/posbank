1663df6d7795b8119113178fbdefc8c8e1fc0ce5
William Maximo <williammaximo@hotmail.com.br>
2016-06-11 16:17:00 -0300

    commit

--- src/main/java/br/com/rp/repository/impl/AgenciaRepositoryImpl.java
+++ src/main/java/br/com/rp/repository/impl/AgenciaRepositoryImpl.java
@@ -1,8 +1,11 @@
 package br.com.rp.repository.impl;
 
+import javax.ejb.Stateless;
+
 import br.com.rp.domain.Agencia;
 import br.com.rp.repository.AgenciaRepository;
 
+@Stateless
 public class AgenciaRepositoryImpl extends AbstractRepositoryImpl<Agencia> implements AgenciaRepository {
 
 	public AgenciaRepositoryImpl() {
