9476886c29e9fcfdee6c6a5cac0b28d76e1ee27f
Felipe Sabadini Facina <fsabadini@hotmail.com>
2016-06-26 11:03:27 -0300

    segurança

---
+++ src/main/java/br/com/rp/seguranca/AcessoNaoPermitidoException.java
@@ -0,0 +1,10 @@
+package br.com.rp.seguranca;
+
+import javax.ejb.ApplicationException;
+
+@ApplicationException
+public class AcessoNaoPermitidoException extends RuntimeException {
+
+	private static final long serialVersionUID = -7791387713812020440L;
+
+}
