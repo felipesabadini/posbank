b99eaa97d9f72f1a1398dbc1dedb92e803a01b5f
William Maximo <williammaximo@hotmail.com.br>
2016-06-26 11:08:36 -0300

    MovimentacaoRest - Consultar Movimentacao Por Conta

--- src/test/java/br/com/rp/rest/MovimentacaoRestTest.java
+++ src/test/java/br/com/rp/rest/MovimentacaoRestTest.java
@@ -19,13 +19,11 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import br.com.rp.AbstractTest;
-import br.com.rp.domain.Cartao;
+import br.com.rp.domain.Conta;
 import br.com.rp.domain.Despesa;
 import br.com.rp.dto.MovimentacaoDTO;
 import br.com.rp.repository.CartaoRepository;
 import br.com.rp.repository.DespesaRepository;
-import br.com.rp.services.DespesaService;
-import br.com.rp.util.Util;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @CleanupUsingScript(value = "db/movimentacaoRest_delete.sql", phase = TestExecutionPhase.AFTER)
@@ -116,5 +114,16 @@ public class MovimentacaoRestTest extends AbstractTest {
 		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
 		List<Despesa> lstDespesa = ((List<Despesa>) response.readEntity(List.class));
 		Assert.assertEquals(5, lstDespesa.size());
+	}
+	
+	@Test
+	@UsingDataSet({"db/cliente.xml", "db/conta.xml", "db/cartao.xml", "db/movimentacao_lista.xml"})
+	public void testeF_consegueConsultarMovimentacaoPorConta() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL + "/movimentacoes/" + CONTA_ID);
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		List<Conta> lstConta = ((List<Conta>) response.readEntity(List.class));
+		Assert.assertEquals(5, lstConta.size());
 	}
 }
