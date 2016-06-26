35d910df42c3f46f57a6d4f6dd4c5a81d74e06b3
Douglas <douglas-da.silva.santos@hotmail.com>
2016-06-26 14:25:50 -0300

    commit

---
+++ src/test/resources/db/devePagarPagamentosAgendados.sql
@@ -0,0 +1,7 @@
+delete from agendamento;
+delete from log_movimentacao;
+delete from movimentacao_resumo;
+delete from movimentacao;
+delete from pagamento;
+delete from conta;
+delete from cliente;
