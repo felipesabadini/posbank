419e9c3ad6e62c6bc794f1a1f794b646368678b8
Douglas <douglas-da.silva.santos@hotmail.com>
2016-06-25 23:35:57 -0300

    Configuracao service

--- src/main/java/br/com/rp/services/impl/GerenteServiceImpl.java
+++ src/main/java/br/com/rp/services/impl/GerenteServiceImpl.java
@@ -1,12 +1,15 @@
 package br.com.rp.services.impl;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.ejb.DependsOn;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import br.com.rp.domain.Cargo;
 import br.com.rp.domain.Proposta;
+import br.com.rp.services.ConfiguracaoService;
 import br.com.rp.services.GerenteService;
 import br.com.rp.services.PropostaService;
 
@@ -16,6 +19,11 @@ public class GerenteServiceImpl implements GerenteService{
 	@EJB
 	PropostaService propostaServiceImpl;
 	
+	@EJB
+	ConfiguracaoService configuracaoService;
+	
+	
+//	procurar como usar o singleton
 	@Override
 	public List<Proposta> visualizarPropostasPorEstado(Cargo cargo, String estado) {
 		if (cargo != Cargo.GERENTE) {
@@ -35,5 +43,24 @@ public class GerenteServiceImpl implements GerenteService{
 		propostaServiceImpl.aceitarProposta(propostaId);
 		
 	}
+
+	@Override
+	public void rejeitarProposta(Cargo cargo, Long funcionarioId, Long propostaId, String motivoRejicao) {
+		
+		if (cargo != Cargo.GERENTE) {
+			return;
+		}
+		
+		propostaServiceImpl.rejeitarProposta(propostaId, funcionarioId, motivoRejicao);		
+	}
+
+	@Override
+	public void alterarHorarioTransacao(Date horaInicialTransacao, Date horaFinalTransacao) {
+
+		configuracaoService.definirHorarioTransacaoBancaria(horaInicialTransacao, horaFinalTransacao);
+		
+	}
+	
+	
 	
 }
--- src/test/java/br/com/rp/service/GerenteServiceTest.java
+++ src/test/java/br/com/rp/service/GerenteServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.rp.service;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -12,20 +13,30 @@ import org.junit.Test;
 
 import br.com.rp.AbstractTest;
 import br.com.rp.domain.Cargo;
+import br.com.rp.domain.Configuracao;
 import br.com.rp.domain.Proposta;
 import br.com.rp.domain.SituacaoProposta;
+import br.com.rp.repository.ConfiguracaoRepository;
 import br.com.rp.repository.PropostaRepository;
 import br.com.rp.services.GerenteService;
+import br.com.rp.util.Util;
 
 public class GerenteServiceTest extends AbstractTest{
 	
-	private static final long PROPOSTA_ID = 1003;
+	private static final long CONFIGURACAO_ID = 1L;
 
+	private static final String MOTIVO_REJEICAO = "Renda Muito baixa";
+
+	private static final long PROPOSTA_ID = 1003;
+	private static final Long FUNCIONARIO_ID = 1000L;
 	@EJB
 	PropostaRepository propostaRepository;
 	
 	@EJB
 	GerenteService gerenteService;
+	
+	@EJB
+	ConfiguracaoRepository configuracaoRepository;
 	
 	@Test
 	@UsingDataSet({"db/cliente.xml", "db/funcionario.xml",
@@ -50,6 +61,32 @@ public class GerenteServiceTest extends AbstractTest{
 		Proposta proposta = propostaRepository.findById(PROPOSTA_ID);
 		
 		Assert.assertEquals(proposta.getSituacao(), SituacaoProposta.AC);
+	}
+	
+	@Test
+	@UsingDataSet({"db/cliente.xml", "db/conta.xml", "db/funcionario.xml",
+	"db/propostas.xml"})
+	@CleanupUsingScript(phase = TestExecutionPhase.AFTER, value={"db/deveAceitarProposta.sql"})
+	public void deveRejeitarProposta() {
+		
+		gerenteService.rejeitarProposta(Cargo.GERENTE, FUNCIONARIO_ID, PROPOSTA_ID, MOTIVO_REJEICAO);
+		
+		Proposta proposta = propostaRepository.findById(PROPOSTA_ID);
+		
+		Assert.assertEquals(proposta.getSituacao(), SituacaoProposta.REG);
+	}
+
+	@Test
+	public void deveAlterarHorarioConfiguracao(){
+		
+		Date horaInicialTransacao = Util.setTime(7, 0);
+		Date horaFinalTransacao = Util.setTime(20, 0);
+		gerenteService.alterarHorarioTransacao(horaInicialTransacao, horaFinalTransacao);
+		
+		Configuracao config = configuracaoRepository.findById(CONFIGURACAO_ID);
+		
+		Assert.assertEquals(config.getHoraInicialTransacao(), horaInicialTransacao);
+		Assert.assertEquals(config.getHoraFinalTransacao(), horaFinalTransacao);
 	}
 
 }
